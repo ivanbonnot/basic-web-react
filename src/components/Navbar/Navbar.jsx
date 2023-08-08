37d6df5e0dc41f67a420b56c9d07541a5ea7c0e9
ivanbonnot <ivanbonnot@gmail.com>
2023-08-08 18:21:03 -0300

    listo navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,27 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import images from '../../constants/images'
+import { GiHamburgerMenu } from 'react-icons/gi'
+import { MdOutlineRestaurantMenu } from 'react-icons/md'
 import './Navbar.css';
 
-const Navbar = () => (
-  <nav className='app__navbar'>
-    <div className='app__navbar-logo'>
-      <img src={images.gericht} alt="app logo" />
-    </div>
+const Navbar = () => {
 
-    <ul className='app__navbar-links'>
-      <li className='p__opensans'><a href="#home">Home</a></li>
-      <li className='p__opensans'><a href="#about">About</a></li>
-      <li className='p__opensans'><a href="#menu">Menu</a></li>
-      <li className='p__opensans'><a href="#awards">Awards</a></li>
-      <li className='p__opensans'><a href="#contact">Contact</a></li>
-    </ul>
+  const [toggleMenu, setToggleMenu] = useState(false)
 
-    <div className='app__navbar-login'>
-      <a href="#login" className='p__opensans'>Log In / Register</a>
-      <div></div>
-      <a href="/" className='p__opensans'>Book Table</a>
-    </div>
-  </nav>
-);
+  return (
+    <nav className='app__navbar'>
+      <div className='app__navbar-logo'>
+        <img src={images.gericht} alt="app logo" />
+      </div>
+
+      <ul className='app__navbar-links'>
+        <li className='p__opensans'><a href="#home">Home</a></li>
+        <li className='p__opensans'><a href="#about">About</a></li>
+        <li className='p__opensans'><a href="#menu">Menu</a></li>
+        <li className='p__opensans'><a href="#awards">Awards</a></li>
+        <li className='p__opensans'><a href="#contact">Contact</a></li>
+      </ul>
+
+      <div className='app__navbar-login'>
+        <a href="#login" className='p__opensans'>Log In / Register</a>
+        <div></div>
+        <a href="/" className='p__opensans'>Book Table</a>
+      </div>
+
+      <div className='app__navbar-smallscreen'>
+        <GiHamburgerMenu color='#fff' fontSize={30} onClick={() => { setToggleMenu(true) }} />
+
+        {toggleMenu && (
+          <div className='app__navbar-smallscreen_overlay flex__center slide-bottom'>
+            <MdOutlineRestaurantMenu color='#fff' fontSize={30} className='overlay__close' onClick={() => { setToggleMenu(false) }} />
+            <ul className='app__navbar-smallscreen_links'>
+              <li className='p__opensans'><a href="#home">Home</a></li>
+              <li className='p__opensans'><a href="#about">About</a></li>
+              <li className='p__opensans'><a href="#menu">Menu</a></li>
+              <li className='p__opensans'><a href="#awards">Awards</a></li>
+              <li className='p__opensans'><a href="#contact">Contact</a></li>
+            </ul>
+          </div>
+        )}
+
+      </div>
+    </nav>
+  )
+}
 
 export default Navbar;
