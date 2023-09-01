6649db6a6637e2e44d85d26e2be58576f6c13191
ivanbonnot <ivanbonnot@gmail.com>
2023-08-31 21:50:56 -0300

    listo, faltan algunos testeos

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -3,11 +3,46 @@ import React from 'react';
 import { FooterOverlay, Newsletter } from '../../components';
 import { FiFacebook, FiTwitter, FiInstagram } from 'react-icons/fi'
 import './Footer.css';
+import images from '../../constants/images';
 
 const Footer = () => (
   <div className='app__footer section__padding'>
     <FooterOverlay />
     <Newsletter />
+
+    <div className='app__footer-links'>
+
+      <div className='app__footer-links_contact'>
+        <h1 className='app__footer-headtext'>Contact Us</h1>
+        <p className='p__opensans'>Complete Address</p>
+        <p className='p__opensans'>1rs phone</p>
+        <p className='p__opensans'>2nd phone</p>
+      </div>
+
+      <div className='app__footer-links_logo'>
+        <img src={images.gericht} alt="footer_logo" />
+        <p className='p__opensans'>"The best way to find yourself is to lose yourself in the service of others"</p>
+        <img src={images.spoon} alt="spoon" className='spoon__img' style={{ marginTop: '50px' }} />
+        <div className='app__footer-links_icons'>
+          <FiFacebook />
+          <FiTwitter />
+          <FiInstagram />
+        </div>
+      </div>
+
+      <div className='app__footer-links_work'>
+        <h1 className='app__footer-headtext'>Working Hours</h1>
+        <p className='p__opensans'>Monday - Friday:</p>
+        <p className='p__opensans'>08:00am - 12:00am </p>
+        <p className='p__opensans'>Saturday - Sunday:</p>
+        <p className='p__opensans'>07:00am - 11:00pm </p>
+      </div>
+
+    </div>
+
+    <div className='footer__copyright' style={{ zIndex: '10' }}>
+      <p className='p__opensans'>2021 Gericht. All Rights reserved</p>
+    </div>
   </div>
 );
 
